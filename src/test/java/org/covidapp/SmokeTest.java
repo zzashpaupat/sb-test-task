0cf0ba645e31d6fb0f3817e532f08bde76c793c4
Oleg Oleynik <oleg.oleynik@inventale.com>
2021-04-04 14:23:40 +0400

    Initial commit

---
+++ src/test/java/org/covidapp/SmokeTest.java
@@ -0,0 +1,23 @@
+package org.covidapp;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+class SmokeTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(SmokeTest.class);
+
+	@Autowired
+	private ApplicationContext context;
+
+	@Test
+	void contextLoads() {
+		logger.info(context.getApplicationName() + " loaded!");
+	}
+
+}
